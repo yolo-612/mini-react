9f7b40d272f8e468d223604ecd878d6b992e05a9
keruisiya <2458097476@qq.com>
2024-04-14 19:47:35 +0800

    feat(useState): 优化不必要的更新

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -296,6 +296,10 @@ function useState(initial){
   currentFiber.stateHooks = stateHooks
 
   function setState(action){
+    // 处理值一样的情况
+    const eagerState = typeof action === "function" ? action(stateHook.state) : action
+    if (eagerState === stateHook.state) return
+    
     // stateHook.state = action(stateHook.state)
     stateHook.queue.push(typeof action === "function" ? action : () => action)
 
